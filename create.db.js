c6c9f3d4a46d693e1e676062ff058e2118eed98c
farhan <farhanafriza24@gmail.com>
2022-06-28 12:14:06 +0700

    add created at and updated at

--- create.db.js
+++ create.db.js
@@ -5,3 +5,7 @@ db.run('CREATE TABLE IF NOT EXISTS author (id TEXT PRIMARY KEY, nama TEXT, jk TE
 db.run('CREATE TABLE IF NOT EXISTS book (isbn TEXT PRIMARY KEY, judul TEXT, author TEXT)')
 //191111291 - Farhan Ismul Afriza
 db.run('CREATE TABLE IF NOT EXISTS users (name TEXT NOT NULL, email TEXT NOT NULL UNIQUE, password TEXT NOT NULL)')
+db.run('ALTER TABLE author ADD COLUMN created_at TEXT')
+db.run('ALTER TABLE book ADD COLUMN created_at TEXT')
+db.run('ALTER TABLE author ADD COLUMN updated_at TEXT')
+db.run('ALTER TABLE book ADD COLUMN updated_at TEXT')
